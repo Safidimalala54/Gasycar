5a6abebe11e47e86facabb11f20ad311aab23855
safidy54 <safidimalalarabezatovo@gmail.com>
2024-01-12 07:04:06 +0000

    main

---
+++ src/main/java/com/gasycar/gasycar/GasycarApplication.java
@@ -0,0 +1,13 @@
+package com.gasycar.gasycar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasycarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasycarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gasycar/gasycar/GasycarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gasycar.gasycar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasycarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
